107011a71e08547bd6cba1173895bbfc428ce328
trungpham291 <144682291+trungpham291@users.noreply.github.com>
2024-05-20 19:42:44 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,58 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        AccountBank account = new AccountBank();
+        Scanner sc = new Scanner(System.in);
+        AccountBank accBank = new AccountBank();
+        ArrayList<AccountBank> banks = new ArrayList<>();
 
         System.out.println("Enter account number: ");
+        String accountNumber = sc.nextLine();
+        System.out.println("Enter account name: ");
+        String accountName = sc.nextLine();
+        System.out.println("Enter account balance: ");
+        double accountBalance = sc.nextDouble();
+        sc.nextLine();
+
+        banks.add(accBank);
+        accBank.setAccountNumber(accountNumber);
+        accBank.setAccountHolderName(accountNumber);
+        accBank.setBalance(accountBalance);
+        System.out.println("Successfully added account !");
+
+
+        while(true){
+            System.out.println("Enter your choice : ");
+            System.out.println("1.Deposit");
+            System.out.println("2.Withdraw");
+            System.out.println("3.Transfer");
+            System.out.println("4.Check Balance");
+            System.out.println("0.Exit");
+            System.out.println("Your choice is:");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch(choice){
+                case 1:
+                    System.out.println("");
+                    break;
+                case 2:
+                    System.out.println("");
+                    break;
+                case 3:
+                    System.out.println("");
+                    break;
+                case 4:
+                    System.out.println("");
+                    break;
+
+            }if(choice == 0) {
+                System.out.println("Thank you for using our system, goodbye and good luck ");
+                break;
+            }
+        }
+
 
     }
 }
